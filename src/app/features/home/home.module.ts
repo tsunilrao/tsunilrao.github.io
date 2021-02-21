81a939c0bf9159bed577cbf3ace3b74ed3fa0df2
tsunilrao <t_sunil_rao@yahoo.com>
2021-02-21 19:10:00 +0530

    home

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { MtxAppBarModule, MtxContainerModule, MtxPageModule, MtxTextModule } from 'mat-ex';
+import { MtxAppBarModule, MtxContainerModule, MtxPageModule, MtxTextModule, MtxToolbarModule } from 'mat-ex';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule, HomeRoutingModule,
-    MtxPageModule, MtxAppBarModule, MtxTextModule, MtxContainerModule
+    MtxPageModule, MtxToolbarModule, MtxAppBarModule, MtxTextModule, MtxContainerModule
   ]
 })
 export class HomeModule { }
