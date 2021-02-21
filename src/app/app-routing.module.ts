e36b55fd69593900bc82d30afbb37a796b5a004d
tsunilrao <t_sunil_rao@yahoo.com>
2021-02-21 22:14:31 +0530

    home path changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: 'prefix' },
-  { path: "home", loadChildren: () => import("./features/home/home.module").then(m => m.HomeModule) }
+  { path: "", loadChildren: () => import("./features/home/home.module").then(m => m.HomeModule) }
 ];
 
 @NgModule({
