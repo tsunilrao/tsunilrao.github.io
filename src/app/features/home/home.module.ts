ca6860a288bbb655f88a3d535ec200925ce7874c
tsunilrao <t_sunil_rao@yahoo.com>
2021-03-03 20:40:59 +0530

    Changed App Bar UI

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { MtxAppBarModule, MtxContainerModule, MtxPageModule, MtxTextModule, MtxToolbarModule } from 'mat-ex';
+import { MtxAppBarModule, MtxContainerModule, MtxPageModule, MtxTextModule, MtxToolbarModule, MtxIconModule } from 'mat-ex';
+import { MatButtonModule } from '@angular/material/button'
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule, HomeRoutingModule,
-    MtxPageModule, MtxToolbarModule, MtxAppBarModule, MtxTextModule, MtxContainerModule
+    MtxPageModule, MtxToolbarModule, MtxAppBarModule, MtxTextModule, MtxContainerModule, MtxIconModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
